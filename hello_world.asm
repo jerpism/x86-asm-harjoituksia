c3939bf4372615ac62ea84e9b349a59ed78eb28b
jerpism <jere.m@live.fi>
2022-11-22 12:10:32 +0200

    siistimistä

--- hello_world.asm
+++ hello_world.asm
@@ -8,14 +8,14 @@ section .text
 
 _start:
 
-    mov eax, 0x4      ; Write() syscall
+    mov eax, 0x4    ; Write() syscall
     mov ebx, 1      ; Tiedosto johon kirjoitetaan (STDOUT)
     mov ecx, msg    ; Kirjoitettava viesti
     mov edx, len    ; viestin pituus
 
     int 0x80        ; käske kerneliä ajamaan write()
 
-    mov al, 1       ; Exit() syscall
+    mov al, 0x1     ; Exit() syscall
     mov ebx, 0      ; statuskoodi 
     int 0x80        ; käske kerenliä ajamaan exit()
 
